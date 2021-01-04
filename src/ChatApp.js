ef61d6e025792b88e758f966fb22babce35dbbe9
mldamico <matias.lioneldamico@gmail.com>
2021-01-04 13:56:20 -0300

    Router and basic pages

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { AppRouter } from "./router/AppRouter";
 
 export const ChatApp = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
 };
