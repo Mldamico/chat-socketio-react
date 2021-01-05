bdfa832cfa2ff754ad274164f6da63e0a60460c5
mldamico <matias.lioneldamico@gmail.com>
2021-01-05 12:00:38 -0300

    Renew token

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useCallback, useState } from "react";
-import { fetchSinToken } from "../helpers/fetch";
+import { fetchConToken, fetchSinToken } from "../helpers/fetch";
 
 export const AuthContext = createContext();
 
@@ -50,7 +50,41 @@ export const AuthProvider = ({ children }) => {
     }
     return resp.msg;
   };
-  const verificaToken = useCallback(() => {}, []);
+  const verificaToken = useCallback(async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setAuth({
+        uid: null,
+        checking: false,
+        logged: false,
+        name: null,
+        email: null
+      });
+      return false;
+    }
+    const resp = await fetchConToken("login/renew");
+    if (resp.ok) {
+      const { usuario } = resp;
+      localStorage.setItem("token", resp.token);
+      setAuth({
+        uid: usuario.uid,
+        checking: false,
+        logged: true,
+        name: usuario.nombre,
+        email: usuario.email
+      });
+      return true;
+    } else {
+      setAuth({
+        uid: null,
+        checking: false,
+        logged: false,
+        name: null,
+        email: null
+      });
+      return false;
+    }
+  }, []);
 
   const logout = () => {};
   return (
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect
 } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 import { ChatPage } from "../pages/ChatPage";
 import { AuthRouter } from "./AuthRouter";
 
 export const AppRouter = () => {
+  const { auth, verificaToken } = useContext(AuthContext);
+
+  useEffect(() => {
+    verificaToken();
+  }, [verificaToken]);
+
+  if (auth.checking) {
+    return <h1>Espere</h1>;
+  }
   return (
     <Router>
       <div>
