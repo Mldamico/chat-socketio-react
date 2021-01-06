5ee4e0d95f59a0584338e54107949d101ab5dfc3
mldamico <matias.lioneldamico@gmail.com>
2021-01-06 10:48:47 -0300

    Chat context and provider

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AuthProvider } from "./auth/AuthContext";
+import { ChatProvider } from "./context/chat/ChatContext";
 import { SocketProvider } from "./context/SocketContext";
 import { AppRouter } from "./router/AppRouter";
 
@@ -7,7 +8,9 @@ export const ChatApp = () => {
   return (
     <AuthProvider>
       <SocketProvider>
-        <AppRouter />
+        <ChatProvider>
+          <AppRouter />
+        </ChatProvider>
       </SocketProvider>
     </AuthProvider>
   );
