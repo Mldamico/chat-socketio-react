cfc6099e224a093844f0b66c95e6ccb801607faf
mldamico <matias.lioneldamico@gmail.com>
2021-01-06 12:48:51 -0300

    message state in sendmessage

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const SendMessage = () => {
+  const [message, setMessage] = useState("");
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (message.length === 0) {
+      return;
+    }
+
+    setMessage("");
+  };
   return (
-    <form className="type_msg row">
+    <form className="type_msg row" onSubmit={onSubmit}>
       <div className="input_msg_write col-sm-9">
-        <input type="text" className="write_msg" placeholder="Mensaje..." />
+        <input
+          type="text"
+          className="write_msg"
+          placeholder="Mensaje..."
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        />
       </div>
       <div className="col-sm-3 text-center">
         <button className="msg_send_btn mt-3" type="submit">
