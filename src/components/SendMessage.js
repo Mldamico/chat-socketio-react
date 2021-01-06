46b0680ef0ae89f812d0992308f2f55256ee55b6
mldamico <matias.lioneldamico@gmail.com>
2021-01-06 13:04:30 -0300

    Send message

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../auth/AuthContext";
+import { ChatContext } from "../context/chat/ChatContext";
+import { SocketContext } from "../context/SocketContext";
 
 export const SendMessage = () => {
   const [message, setMessage] = useState("");
+  const { socket } = useContext(SocketContext);
+  const { auth } = useContext(AuthContext);
 
+  const { chatState } = useContext(ChatContext);
   const onSubmit = e => {
     e.preventDefault();
     if (message.length === 0) {
       return;
     }
-
+    socket?.emit("mensaje-personal", {
+      de: auth.uid,
+      para: chatState.chatActivo,
+      mensaje: message
+    });
     setMessage("");
   };
   return (
